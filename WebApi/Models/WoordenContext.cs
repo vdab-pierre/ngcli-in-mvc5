acff1bb6aa6291c317fce293833cb92faf16aa2c
vdab-pierre somerstraat <pierre.colin@vdab.be>
2019-02-07 16:36:58 +0100

    webapi, entityfwk code first en models

---
+++ WebApi/Models/WoordenContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using WebApi.Models.latijn.woorden;
+namespace WebApi.Models
+{
+    public class WoordenContext:DbContext
+    {
+        public DbSet<Woord> Woorden { get; set; }
+    }
+}
---
+++ WebApi/Models/latijn/woorden/Woord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Models.latijn.woorden
+{
+    public enum Genus
+    {
+        m,v,o
+    }
+    public class Woord
+    {
+        public int Id { get; set; }
+        public string GrondWoord { get; set; }
+        public Genus Genus { get; set; }
+        public virtual IEnumerable<Vertaling> Vertalingen { get; set; }
+        public virtual IEnumerable<AanvullendGegeven> AanvullendeGegevens { get; set; }
+    }
+}
